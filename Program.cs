fac022d6ef922920b70e8da6f2f5fc52baf1ff0c
Purva Pawar <pawarpurva255@gmail.com>
2025-01-25 20:41:10 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string temp = "Namade";
+
+            string uppr = temp.ToUpper();
+
+            char[] arg = uppr.ToCharArray();
+
+            string revers = "";
+
+            for (int i = arg.Length - 1; i > -1; i--)
+            {
+                revers += arg[i];
+            }
+
+            if (revers == uppr)
+            {
+                Console.WriteLine("This is palindrom:" + revers);
+            }
+            else
+            {
+                Console.WriteLine("Not a Palindrom:"  + revers);
+            }
+        }
+    }
+}
